b1b246733c962dd2e17b1fa5580967ad11db6d3c
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2021-03-02 14:09:46 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "PrintABrick",
+  "description": "Web catalogue of LEGO® parts for 3D printing",
+  "repository": "https://github.com/artificialbutter/PrintABrick/",
+  "logo": "https://www.ldraw.org/common/images/logos/favicons/LDraw_Green_64x64.png",
+  "keywords": ["LEGO", "PrintABrick", "3D"]
+}
